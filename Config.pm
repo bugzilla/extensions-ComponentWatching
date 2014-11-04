554cb7cc1af79e00523e9a0dc4a17c31513b60b4
Gervase Markham <gerv@gerv.net>
2014-11-04 13:29:47 +0000

    Initial version of ComponentWatching, compatible with Bugzilla 4.2 and 4.4.

---
+++ Config.pm
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
+package Bugzilla::Extension::ComponentWatching;
+use strict;
+use constant NAME => 'ComponentWatching';
+
+__PACKAGE__->NAME;
